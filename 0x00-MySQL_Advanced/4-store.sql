f41d6caba51d522bcf43b85711a6e80b5fb732ae
Niceblueman <wintheiser761@gmail.com>
2024-01-18 23:56:45 +0100

    -- reduce_quantity

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,12 @@
--- triggers
+-- reduce_quantity
 DROP TRIGGER IF EXISTS reduce_quantity;
-
-DELIMITER $$ CREATE TRIGGER reduce_quantity AFTER INSERT ON orders FOR EACH ROW BEGIN
-UPDATE items
-SET
-    quantity = quantity - NEW.number
-WHERE
-    name = NEW.item_name;
-
-END $$ DELIMITER;
+DELIMITER $$ 
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders 
+FOR EACH ROW 
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$ 
+DELIMITER ;
