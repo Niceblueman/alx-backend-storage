82b27e866651815c620fb0887c5786722627ea50
Niceblueman <wintheiser761@gmail.com>
2024-01-18 23:58:19 +0100

    -- need_meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,15 +1,10 @@
--- view with function
+-- need_meeting
 DROP VIEW IF EXISTS need_meeting;
-
-CREATE VIEW
-    need_meeting AS
-SELECT
-    name
-FROM
-    students
-WHERE
-    score < 80
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
     AND (
         last_meeting IS NULL
-        OR last_meeting < SUBDATE (CURRENT_DATE(), INTERVAL 1 MONTH)
+        OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
     );
